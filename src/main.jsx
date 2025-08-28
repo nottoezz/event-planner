e969ee5271b068f7b39fc0ab9fa1f0cdd2e0d101
nottoezz <liam@birch.co.za>
2025-08-28 17:14:35 +0200

    chore(app): wrap app with EventsProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider.jsx";
+import EventsProvider from "./contexts/EventsProvider.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <EventsProvider>
+          <App />
+        </EventsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
