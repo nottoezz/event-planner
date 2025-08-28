dde1a99ea57ff2e9c99a2bc34f104014b4a107ab
nottoezz <liam@birch.co.za>
2025-08-28 21:50:49 +0200

    Updated for github deployment to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider.jsx";
 import EventsProvider from "./contexts/EventsProvider.jsx";
 import App from "./App.jsx";
@@ -8,12 +8,12 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <EventsProvider>
           <App />
         </EventsProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
